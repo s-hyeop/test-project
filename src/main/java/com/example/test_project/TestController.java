6ec36c2552f3a330e5e27315b2d0c472e96d7062
song-laptop <tmdguq0330@gmail.com>
2025-08-16 02:18:09 +0900

    devtools 추가, render-schema 삭제, TestController 추가

---
+++ src/main/java/com/example/test_project/TestController.java
@@ -0,0 +1,14 @@
+package com.example.test_project;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class TestController {
+
+  @GetMapping("/test")
+  public String getMethodName() {
+    return "test";
+  }
+}
