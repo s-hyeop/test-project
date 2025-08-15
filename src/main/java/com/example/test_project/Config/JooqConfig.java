f8aa25d0d4145d4c227f3128ea645319879640b8
song-laptop <tmdguq0330@gmail.com>
2025-08-16 02:24:56 +0900

    JooqConfig 추가

---
+++ src/main/java/com/example/test_project/Config/JooqConfig.java
@@ -0,0 +1,15 @@
+package com.example.test_project.Config;
+
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqConfig {
+    @Bean
+    public DefaultConfigurationCustomizer jooqDefaultConfigurationCustomizer() {
+        return c -> c.settings()
+            .withRenderFormatted(true) // 로그에 쿼리 예쁘게 출력하기 
+            .withRenderSchema(false); // 로그에 스키마 빼고 출력하기
+    }
+}
