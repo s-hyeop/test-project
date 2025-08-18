b1376a3585cd91663e4250950ed8b9f36a1214c3
song-laptop <tmdguq0330@gmail.com>
2025-08-18 19:21:11 +0900

    add [Controller,Service,Repository][DI,Annotation]

--- src/main/java/com/example/test_project/api/todo/TodoRepository.java
+++ src/main/java/com/example/test_project/api/todo/TodoRepository.java
@@ -1,8 +1,17 @@
 package com.example.test_project.api.todo;
 
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
+import com.example.jooq.generated.tables.daos.UsersDao;
+
+import lombok.RequiredArgsConstructor;
+
 @Repository
+@RequiredArgsConstructor
 public class TodoRepository {
+
+    private final DSLContext dsl;
+    private final UsersDao usersDao;
     
 }
