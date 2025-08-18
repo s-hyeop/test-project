29b1c9428e68bdcf4480240cd7756d6a91bfcb90
song-laptop <tmdguq0330@gmail.com>
2025-08-18 19:15:31 +0900

    add [Controller,Service,Repository][DI,Annotation]

--- src/main/java/com/example/test_project/api/auth/AuthService.java
+++ src/main/java/com/example/test_project/api/auth/AuthService.java
@@ -1,5 +1,67 @@
 package com.example.test_project.api.auth;
 
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+
+import com.example.test_project.api.auth.dto.request.EmailValidateRequest;
+import com.example.test_project.api.auth.dto.request.LoginRequest;
+import com.example.test_project.api.auth.dto.request.ResetPasswordCodeSendRequest;
+import com.example.test_project.api.auth.dto.request.ResetPasswordCodeVerifyRequest;
+import com.example.test_project.api.auth.dto.request.ResetPasswordRequest;
+import com.example.test_project.api.auth.dto.request.SignupCodeSendRequest;
+import com.example.test_project.api.auth.dto.request.SignupCodeVerifyRequest;
+import com.example.test_project.api.auth.dto.request.SignupRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
-    
+
+    private final DSLContext dsl;
+
+    public void validateEmail(EmailValidateRequest emailValidateRequest) {
+
+    }
+
+    public void login(LoginRequest loginRequest) {
+
+    }
+
+    public void sendSignupCode(SignupCodeSendRequest signupCodeSendRequest) {
+
+    }
+
+    public void verifySignupCode(SignupCodeVerifyRequest signupCodeVerifyRequest) {
+
+    }
+
+    public void signup(SignupRequest signupRequest) {
+
+    }
+
+    public void sendResetPasswordCode(ResetPasswordCodeSendRequest resetPasswordCodeSendRequest) {
+
+    }
+
+    public void verifyResetPasswordCode(ResetPasswordCodeVerifyRequest resetPasswordCodeVerifyRequest) {
+
+    }
+
+    public void resetPassword(ResetPasswordRequest resetPasswordRequest) {
+
+    }
+
+    public void getTokens(int userNo) {
+
+    }
+
+    public void refreshAccessToken(int userNo, String refreshToken) {
+
+    }
+
+    public void deleteToken(int userNo, String refreshToken) {
+
+    }
+
 }
--- src/main/java/com/example/test_project/api/user/UserService.java
+++ src/main/java/com/example/test_project/api/user/UserService.java
@@ -1,5 +1,26 @@
 package com.example.test_project.api.user;
 
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
-    
+
+    private final DSLContext dsl;
+
+    public void getUserDetail(int userNo) {
+
+    }
+
+    public void updateUser(int userNo) {
+
+    }
+
+    public void changePassword(int userNo) {
+
+    }
+
 }
