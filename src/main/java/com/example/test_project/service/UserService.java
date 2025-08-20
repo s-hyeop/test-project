108a95f764ca11fd8fdc14b7a8e5a5cb789e8471
song-laptop <tmdguq0330@gmail.com>
2025-08-21 02:06:47 +0900

    UserService 표기 및 변수명 변경

--- src/main/java/com/example/test_project/service/UserService.java
+++ src/main/java/com/example/test_project/service/UserService.java
@@ -17,42 +17,45 @@ public class UserService {
     private final UsersRepository usersRepository;
 
     public UserDetailResponse getUserDetail(int userNo) {
-        Users users = usersRepository.find(userNo)
-            .orElseThrow(() -> new RuntimeException("회원을 찾을 수 없음.")); // TODO: 예외 후처리 필요
+        Users userPojo = usersRepository.find(userNo).orElseThrow(() ->
+            new RuntimeException("회원을 찾을 수 없음.")  // TODO: 예외 후처리 필요
+        );
 
         return UserDetailResponse.builder()
-            .email(users.getEmail())
-            .userName(users.getUserName())
-            .createdAt(users.getCreatedAt())
+            .email(userPojo.getEmail())
+            .userName(userPojo.getUserName())
+            .createdAt(userPojo.getCreatedAt())
             .build();
         }
 
     public void updateUser(int userNo, UserPatchRequest userPatchRequest) {
-        usersRepository.find(userNo)
-            .orElseThrow(() -> new RuntimeException("회원을 찾을 수 없음.")); // TODO: 예외 후처리 필요
+        usersRepository.find(userNo).orElseThrow(() ->
+            new RuntimeException("회원을 찾을 수 없음.")  // TODO: 예외 후처리 필요
+        );
 
-        Users userPojo = new Users();
-        userPojo.setUserName(userPatchRequest.getUserName());
+        Users updateUserPojo = new Users();
+        updateUserPojo.setUserName(userPatchRequest.getUserName());
 
-        if (usersRepository.update(userNo, userPojo) == 0) {
+        if (usersRepository.update(userNo, updateUserPojo) == 0) {
             new RuntimeException("회원 정보 수정에 실패했음"); // TODO: 예외 후처리 필요
         }
     }
 
     public void changePassword(int userNo, UserChangePasswordRequest userChangePasswordRequest) {
-        Users users = usersRepository.find(userNo)
-            .orElseThrow(() -> new RuntimeException("회원을 찾을 수 없음.")); // TODO: 예외 후처리 필요
-
+        Users userPojo = usersRepository.find(userNo).orElseThrow(() ->
+            new RuntimeException("회원을 찾을 수 없음.")  // TODO: 예외 후처리 필요
+        );
+        
         // DOTO: Spring Security 작업 후 해싱 처리 및 비교 추가 필요
-        if (userChangePasswordRequest.getPassword() != users.getPassword()) {
+        if (userChangePasswordRequest.getPassword() != userPojo.getPassword()) {
             new RuntimeException("비밀번호가 일치하지 않음."); // TODO: 예외 후처리 필요
         }
         String passwordHash = userChangePasswordRequest.getNewPassword(); // TODO: Spring Security 작업 후
 
-        Users userPojo = new Users();
-        userPojo.setPassword(passwordHash);
+        Users updateUserPojo = new Users();
+        updateUserPojo.setPassword(passwordHash);
 
-        if (usersRepository.update(userNo, userPojo) == 0) {
+        if (usersRepository.update(userNo, updateUserPojo) == 0) {
             new RuntimeException("비밀번호 변경에 실패했음"); // TODO: 예외 후처리 필요
         }
     }
